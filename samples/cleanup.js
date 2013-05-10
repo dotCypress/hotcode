aa569cbe32b2f3b9cd651dda58927b9fce6669a8
Vitaly Domnikov <dotcypress@gmail.com>
2013-05-10 15:53:55 +0300

    Cleanup script

--- samples/cleanup.js
+++ samples/cleanup.js
@@ -1,1 +1,8 @@
-print('Cleaning...');
+var hotcode = new Mongo().getDB('hotcode');
+var albumImages = hotcode.albums.find().toArray().reduce(function(a, b) {
+    return a.concat(b.images);
+}, []);
+
+print("Initial amount: ", hotcode.images.count());
+hotcode.images.remove({ _id: { $not: { $in: albumImages }}});
+print("Result amount: ", hotcode.images.count());
