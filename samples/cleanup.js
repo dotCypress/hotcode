714f9293b43bf1d370c894a341f215a73146f102
Vitaly Domnikov <dotcypress@gmail.com>
2013-05-15 14:57:12 +0300

    Samples

--- samples/cleanup.js
+++ samples/cleanup.js
@@ -1,8 +1,12 @@
 var hotcode = new Mongo().getDB('hotcode');
-var albumImages = hotcode.albums.find().toArray().reduce(function(a, b) {
-  return a.concat(b.images);
-}, []);
+var albumImages = hotcode.albums.
+  find().
+  toArray().
+  reduce(function(a, b) {
+    return a.concat(b.images);
+  }, []);
 
 print("Initial amount: ", hotcode.images.count());
-hotcode.images.remove({ _id: { $not: { $in: albumImages }}});
+var selector = { _id: { $not: { $in: albumImages }}};
+hotcode.images.remove(selector);
 print("Result amount: ", hotcode.images.count());
--- samples/report.js
+++ samples/report.js
@@ -1,10 +1,8 @@
 var hotcode = new Mongo().getDB('hotcode');
 var data = hotcode.albums.find().toArray().reduce(function(a, b) {
   a.push([b._id, b.images.length]);
-  return a;
-}, []);
+  return a; }, []);
 print("Album,Photos");
-data.forEach(function(infoArray, index){
-   dataString = infoArray.join(",");
-   print(dataString);
+data.forEach(function(infoArray){
+   print(infoArray.join(","));
 });
